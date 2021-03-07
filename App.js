2d8425f606f26e59302a12177e58af7a5df3b5cd
esq805 <eileensquiroz@gmail.com>
2021-03-07 15:14:28 -0800

    Updated Screen

--- App.js
+++ App.js
@@ -1,26 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
-import { Text, View, Image, TouchableOpacity } from 'react-native';
-import styles from './src/pages/styles';
+import * as React from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import styles from './src/styles/SplashScreenStyles';
 
-//export default function App() {
-export default class App extends Component {
-  render () {
+function NewUser({ navigation }) {
   return (
     <View style={styles.container}>
-      <StatusBar style='auto'/>
-      <Text style={styles.txtMain}>Welcome to the Medical Aid App</Text>
-      <Image 
-        style={styles.imgMain}
-        source={require('./src/image/AppIcon.png')}
-      />
-       <TouchableOpacity
-         style={styles.btnCont}
-         onPress={()=> console.log('HELLO')}>
-          <Text style={styles.btnTxt}> 
-            Click to continue 
-          </Text>
+    <Text style={styles.txtTitle}>Welcome to the Medical Aid App</Text>
+    <Image 
+      style={styles.imgMain}
+      source={require('./src/image/Logo.png')} />
+     <TouchableOpacity 
+        style={styles.btnGreen}
+        onPress={()=>console.log('LOGIN')}>
+        <Text style={styles.btnTxt}>
+          Log In
+        </Text>
       </TouchableOpacity>
-    </View>
+      <TouchableOpacity 
+        style={styles.btnWhite}
+        onPress={()=>console.log('CREATEANACCOUNT')}>
+        <Text style={styles.btnTxt}>  
+          Create an account
+        </Text>
+      </TouchableOpacity>
+  </View>
+  )
+}
+
+// function NewUser({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       {/* <TouchableOpacity 
+//         style={styles.btnGreen}
+//         onPress={()=>console.log('LOGIN')}>
+//         <Text style={styles.btnTxt}>
+//           Log In
+//         </Text>
+//       </TouchableOpacity>
+//       <TouchableOpacity 
+//         style={styles.btnWhite}
+//         onPress={()=>console.log('CREATEANACCOUNT')}>
+//         <Text style={styles.btnTxt}>  
+//           Create an account
+//         </Text>
+//       </TouchableOpacity> */}
+//     </View>
+//   );
+// }
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="New">
+        <Stack.Screen name="New" component={NewUser} options={{headerShown: false}}/>
+        {/* <Stack.Screen name="New" component={NewUser} options={{title: ''}}/> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}}
+}
+
+export default App;
