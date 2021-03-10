2e826a4facb1496a313404c4f6620035e47cdfb0
esq805 <eileensquiroz@gmail.com>
2021-03-09 17:58:44 -0800

    Splash Screen changed to a Gradient

--- android/app/src/main/java/com/medicalaid/MainActivity.java
+++ android/app/src/main/java/com/medicalaid/MainActivity.java
@@ -11,13 +11,13 @@ import expo.modules.splashscreen.singletons.SplashScreen;
 import expo.modules.splashscreen.SplashScreenImageResizeMode;
 
 public class MainActivity extends ReactActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-    // SplashScreen.show(...) has to be called after super.onCreate(...)
-    // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
-    SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, ReactRootView.class, false);
-  }
+//   @Override
+//   protected void onCreate(Bundle savedInstanceState) {
+//     super.onCreate(null);
+//     // SplashScreen.show(...) has to be called after super.onCreate(...)
+//     // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
+//     SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, ReactRootView.class, false);
+//   }
 
 
     /**
