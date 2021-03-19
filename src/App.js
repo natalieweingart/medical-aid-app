202ed986ba2ba042f33e0471fb1612edb36ab651
esq805 <eileensquiroz@gmail.com>
2021-03-19 13:50:00 -0700

    Navigate Update more organized

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import * as React from 'react';
 import { AppNavigator } from './routes/NavigateStart';
 
 function App() {
+  // IF logged in 
+  //    if the account is Patient
+  //        go to the Patient route
+  //    else if the acc is CT
+  //        go to Caretaker Route
+  // else new user navigater 
   return <AppNavigator />;
 }
 
